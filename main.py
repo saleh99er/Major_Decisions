4be284170597aefdd04f564b79c0a33fe575ce3a
Saleh Hassen <saleh.i.hassen@gmail.com>
2017-08-10 22:56:58 -0400

    setting up questions

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 #backbone of app
 print "WIP"
+
+#initialize Major Decisions
+
+
+
+#start page "Major Decisions" with two buttons to start and quit it
+#throughout the program their needs to be an option to quit
+
+
+
+#first question: What is their intended major then calculates with this
+# and moves on to next one if answer is valid otherwise requires a new response
+# Info extracted from this, length of the string they gave ranging from 3 to 16
+
+
+
+#second question: What state do they live in
+# Info extracted from this, whether or not an "a" is present in the state name or abbrev they give
+
+
+
+
+#third question: Blah Blah Blah
+
+
+
+
+#select a random value
+
+
+
+#third question:
