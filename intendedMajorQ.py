bc38ae33c99da32817495851892a9434fe4951ee
Kwami2 <akk83@Cornell.edu>
2017-08-12 00:37:28 -0400

    Updated Version

--- intendedMajorQ.py
+++ intendedMajorQ.py
@@ -3,14 +3,16 @@
 # and moves on to next one if answer is valid otherwise requires a new response
 # Info extracted from this, length of the string they gave ranging from 3 to 16
 
-major= raw_input("What is your intended major? ")
-def intendedMajorQ(major):
+def intendedMajorQ():
   """The first question asks the user for their intended major, and if the inptu is valid, stores the length of the string as a value"""
+  major= raw_input("What is your intended major? ")
   if len(major)>0 and major.isalpha():
     value_1=len(major)
   else:
     print "This is not a valid major. Please enter a valid one"
-    intendedMajorQ(major)
+    intendedMajorQ()
   if value_1 < 3 or value_1 > 16:
-        random.randint(3,16)
-Value_1=intendedMajorQ(major)
+    Back_up_value=random.randint(3,16)
+    return Back_up_value
+  else:
+    return value_1
