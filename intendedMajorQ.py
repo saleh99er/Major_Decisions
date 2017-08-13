5875697792a8de0f6d07001e925bda4155d788fd
Kwami2 <akk83@Cornell.edu>
2017-08-13 10:50:27 -0400

    Turned this function into a while-loop. But haven't fixed the last part tho

--- intendedMajorQ.py
+++ intendedMajorQ.py
@@ -9,38 +9,45 @@ def intendedMajorQ():
   # and moves on to next one if answer is valid otherwise requires a new response
   # Info extracted from this, length of the string they gave ranging from 3 to 16
   
+  #noNumber s is used to check if the input is valid
+  # validInput and Back_up_value are used for the condition of the wile loop and are the return values. 
   noNumbers = True
   validInput=0
-  # A while loop that keeps stating that an input is false if the iser does not input a valid input. 
-  while validInput==0:
-    """The first question asks the user for their intended major, and if the input is valid, stores the length of the string as a value"""
-    major= raw_input("What is your intended major? ")
-    #for-loop here that will check for any number(string) within the major.
-    for i in range(len(major)):
-      for j in range(10):
+  Back_up_value=0
+  
+  # A while loop that keeps stating that an input is false if the user does not input a valid input. This will be added in the main.py.
+  #The while statment will look something like:
+    #while validInput=0 and Back-up-value=0
+    
+  #The first question asks the user for their intended major, and if the input is valid, stores the length of the string as a value"""
+  major= raw_input("What is your intended major? ")
+  
+  #for-loop here that will check for any number(string) within the major.
+  for i in range(len(major)):
+    for j in range(10):
         if major[i]==str(j):
           noNumbers=False
-    if noNumbers==False:
-      print "There are no majors that exist that have numbers in them. Please enter a valid major."
-      continue
-    #Another for-loop that checks if any number(int) is within the major.
-    if NoNumbers==True: 
-      for i in range(len(major)):
-        for j in range(10):
-          if major[i]==j:
-             noNumbers=False
-    if noNumbers==False:
-      print "There are no majors that exist that have numbers in them. Please enter a valid major."
-      continue
-    if noNumbers==True and type(major)==str and len(major)>0:
-      value_1=len(major)
-      validInput=1
-    else:
-      print "This is not a valid major. Please enter a valid one."
-
-  if value_1 < 3 or value_1 > 16:
+  if noNumbers==False:
+    print "There are no majors that exist that have numbers in them. Please enter a valid major."
+    
+  #Another for-loop that checks if any number(int) is within the major.
+  if NoNumbers==True: 
+    for i in range(len(major)):
+      for j in range(10):
+         if major[i]==j:
+            noNumbers=False
+  if noNumbers==False:
+    print "There are no majors that exist that have numbers in them. Please enter a valid major."
+    
+  # The final cehck, if the input passes this, a value for the input is returned. 
+  if noNumbers==True and type(major)==str and len(major)>0:
+    validInput=len(major)
+  else:
+    print "This is not a valid major. Please enter a valid one."
+  # A back up if-statement in the case a value that is not expected is generated. 
+  if validInput < 3 or validInput> 16:
     Back_up_value=randint(3,16) #randint includes values 3 and 16 but your conditional is exclusive, choose one or the other please
                                 #Unsure of what Saleh meant: Do reseacrh into randint. 
     return Back_up_value
   else:
-    return value_1
+    return validInput
