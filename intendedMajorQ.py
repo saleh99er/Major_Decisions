817119103b550c07d0748ed32c3e780a0510245c
Saleh Hassen <saleh.i.hassen@gmail.com>
2017-08-13 11:14:31 -0400

    please read recommended changes

--- intendedMajorQ.py
+++ intendedMajorQ.py
@@ -5,23 +5,26 @@ pass #Do research into the concept of "pass". Never though I had to ever write t
 from random import randint
 
 def intendedMajorQ():
-  #first question: What is their intended major then calculates with this
-  # and moves on to next one if answer is valid otherwise requires a new response
-  # Info extracted from this, length of the string they gave ranging from 3 to 16
-  """The first question asks the user for their intended major, and if the input is valid, stores the length of the string as a value"""
+  """The first question asks the user for their intended major, and if the input is valid, stores the length of the string as a value
+
+  first question: What is their intended major then calculates with this
+  and moves on to next one if answer is valid otherwise requires a new response
+  Info extracted from this, length of the string they gave ranging from 3 to 16
+  """
   major= raw_input("What is your intended major? ")
   
   noNumbers = True
-   
+  Left all my changes like this so you guys can see it
   #add for-loop here that will check for any number(string) within the major. 
   #If there isn't, then noNumbers will reamain true.
   #Else statement will be "print "This is not a valid major. Please enter a valid one."" and NoNumbers=False
   #Use a nested for-loop.
-    #The first one will be for the index(ranging from 0 to (len(major-1)))
-    #The second will be for the numbers(string)(Ranging from 0 to 9)
-    #Example: if major(i)=str(j):
+    THERE IS NO NEED FOR THIS LOOP, a while loop is needed to force the user to reenter the data if invalid #The first one will be for the index(ranging from 0 to (len(major-1)))
+    use range and for loop to get these values then cast to a string and then track the string with:#The second will be for the numbers(string)(Ranging from 0 to 9)
+    substring in string if this boolean expression is true, then set noNumbers = False.
     #noNumbers=False 
-  
+
+  THIS LOOP IS UNNECCESSARY
   #Make another for-loop that checks if any number(int) is within the major.
   #If a number(int) is found, then noNumbers=False
   #Else Statment will be noNumbers will remain True and print "This is not a valid major. Please enter a valid one."
@@ -37,8 +40,7 @@ def intendedMajorQ():
 
 
   if value_1 < 3 or value_1 > 16:
-    Back_up_value=randint(3,16) #randint includes values 3 and 16 but your conditional is exclusive, choose one or the other please
-                                #Unsure of what Saleh meant: Do reseacrh into randint. 
+    Back_up_value=randint(3,16) randint will select a random number between 3 and 16 inclusively but in line 42 you are exclusive (not including the values 3 and 16). To make the numbers selected fair please make the comparison statements use <= >= so those numbers are included as okay or change the random range to 4 to 15 (which is what I least prefer since art is a major and its 3 letters long)
     return Back_up_value
   else:
     return value_1
