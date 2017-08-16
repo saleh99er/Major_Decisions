d71bcb4d618b06b9b2f6e2f187ad3d6c1ba89bbe
Kwami2 <akk83@Cornell.edu>
2017-08-15 22:01:24 -0400

    I made it into function

--- Input_Checker.py
+++ Input_Checker.py
@@ -1,24 +1,17 @@
-#Script Descritption:
-  #This script is meant to be place into any function that uses a raw_input.
-  #This script check that there is no numbers withi the input and that it is a string.
-  # Look at "intendedMajorQ.py" for an example.
-#We start by assuming that there is no numbers witin the string nd that the input is a string.
-noNumbers=True
+#Function Descritption:
+  #This Function is meant to be place into any function that uses a raw_input.
+  #This Function check that there is no numbers within the input and that it is a string.
 
-#THE "INPUT" VARIABLE IS MEANT TO CHNAGE DEPENDIN OF THE FUNCTION THAT IT'S BEING APPLIED TO
-
-#Nested for loop checks if the is a number(string) in any of the indexes
-for i in range(len(INPUT)):
-  for j in range(10):
-    if INPUT[i]==str(j):
-      noNumbers = False
-
-if noNumbers == False:
-  print "Whatever Statment that you want if there are numbers(string) within the string"
-# The final cehck, if the input passes this, a value for the input is returned. 
-if noNumbers==True and type(INPUT)==str and len(INPUT)>0:
-  validInput=len(INPUT)
-  print "Whatever statement you want"
-else:
-  print "Whatever statement you want"
-  validInput = # Use a recurive statement
+def Input_Checker(INPUT):
+  #We start by assuming that there is no numbers witin the string nd that the input is a string.
+  noNumbers=True
+  #THE "INPUT" VARIABLE IS MEANT TO CHNAGE DEPENDIN OF THE FUNCTION THAT IT'S BEING APPLIED TO
+  if type(INPUT)==str and len(INPUT)>0:
+    if "0" in INPUT or "1" in INPUT or "2" in INPUT or "3" in INPUT or "4" in INPUT or "5" in INPUT or "6" in INPUT or "7" in INPUT or "8" in INPUT or "9" in INPUT: 
+      noNumbers=False
+      return noNumbers
+    else:
+      return noNumbers
+  else:
+    noNumbers=False
+    return noNumbers
