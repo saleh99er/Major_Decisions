bca5637b39da9955f1d324f8d0e9a8e9c7db535c
Kwami2 <akk83@Cornell.edu>
2017-08-16 12:06:40 -0400

    Made the more efficent python loop

--- Input_Checker.py
+++ Input_Checker.py
@@ -1,17 +1,11 @@
 #Function Descritption:
-  #This Function is meant to be place into any function that uses a raw_input.
+  #This Function is meant to be placed into any function that uses a raw_input.
   #This Function check that there is no numbers within the input and that it is a string.
-
 def Input_Checker(INPUT):
   #We start by assuming that there is no numbers witin the string nd that the input is a string.
+  list_of_numbers=["0","1","2","3","4","5","6","7","8","9"]
   noNumbers=True
-  #THE "INPUT" VARIABLE IS MEANT TO CHNAGE DEPENDIN OF THE FUNCTION THAT IT'S BEING APPLIED TO
-  if type(INPUT)==str and len(INPUT)>0:
-    if "0" in INPUT or "1" in INPUT or "2" in INPUT or "3" in INPUT or "4" in INPUT or "5" in INPUT or "6" in INPUT or "7" in INPUT or "8" in INPUT or "9" in INPUT: 
+  for i in range(len(list_of_numbers)):
+    if INPUT[i]==list_of_numbers[i] or len(INPUT)<=0:
       noNumbers=False
-      return noNumbers
-    else:
-      return noNumbers
-  else:
-    noNumbers=False
-    return noNumbers
+  return noNumbers
