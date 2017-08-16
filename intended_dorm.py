ad4f67b5ce9a87de924baf96108455fd7a61a7d0
Atsutse <akk72@cornell.edu>
2017-08-15 23:03:09 -0400

    PLEASE CHECK

--- intended_dorm.py
+++ intended_dorm.py
@@ -1,37 +1,49 @@
 from random import random
 def intended_dorm():
-  dorm = raw_input("Where will you be living? ")
-  dorm = dorm.lower()
-  a = ['mews','mews hall']
-  b = ['townhouse','townhouse community']
-  c = ['balch','balch hall']
-  d = ['clara dickson','clara dickson hall']
-  e = ['bauer','court-kay-bauer','court kay bauer','court-kay-bauer hall','court kay bauer hall']
-  f = ['high rise','high rise','high rise #5','high rise #5']
-  g = ['jameson','jameson hall']
-  h = ['low rise','low rise','low-rise #6','low-rise #6','low rise #6','low rise #6','low-rise #6','low-rise #6']
-  i = ['low-rise #7','low-rise #7','low rise #7','low rise #7','low-rise #7','low-rise #7']
-  j = ['mary donlon','mary donlon hall','mary','mary hall','donlon','donolon hall']
-
-  if dorm == a:
-    return random()*1
-  elif dorm == b:
-    return random()*1
-  elif dorm == c:
-    return random()*1
-  elif dorm == d:
-    return random()*1
-  elif dorm == e:
-    return random()*1
-  elif dorm == f:
-    return random()*1
-  elif dorm == g:
-    return random()*1
-  elif dorm == h:
-    return random()*1
-  elif dorm == i:
-    return random()*1
-  elif dorm == j:
-    return random()*1
+  valid_input = False
+  while valid_input == False:
+    dorm = raw_input("Where will you be living?")
+    dorm = dorm.lower()
+    a = ['mews','mews hall']
+    b = ['townhouse','townhouse community']
+    c = ['balch','balch hall']
+    d = ['clara dickson','clara dickson hall']
+    e = ['bauer','court-kay-bauer','court kay bauer','court-kay-bauer hall','court kay bauer hall']
+    f = ['high rise','high rise','high rise #5','high rise #5']
+    g = ['jameson','jameson hall']
+    h = ['low rise','low rise','low-rise #6','low-rise #6','low rise #6','low rise #6','low-rise #6','low-rise #6']
+    i = ['low-rise #7','low-rise #7','low rise #7','low rise #7','low-rise #7','low-rise #7']
+    j = ['mary donlon','mary donlon hall','mary','mary hall','donlon','donolon hall', 'egg']
+    if dorm == a in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == b in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == c in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == d in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == e in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == f in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == g in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == h in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == i in dorm:
+      return random()*.3
+      valid_input = True
+    elif dorm == j in dorm:
+      return random()*.3
+      valid_input = True
   else:
     print 'invalid response'
+    
