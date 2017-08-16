10868b13a6a25782a60819c7b1676e6797f75cf8
Kwami2 <akk83@Cornell.edu>
2017-08-16 12:07:33 -0400

    Very minor changes from what you pulled earlier

--- realwhereareyoufromQ.py
+++ realwhereareyoufromQ.py
@@ -7,7 +7,7 @@ def realwhereareyoufromQ(is_US,State,Country):
       print "%s not a valid state. Please enter a valid one " % State
       print " \n "
       new_state= raw_input("What state are you from? ")
-      retval=realwhereareyoufrom("Y",new_state, USA)
+      retval=realwhereareyoufrom("Y",new_state,"USA")
       return retval
     else:
       for i in len(State):
@@ -23,14 +23,14 @@ def realwhereareyoufromQ(is_US,State,Country):
             return uniform(.25,.3)
           else:
             return uniform(.0,.3)
-  if is_US=="N" or is_US=="Yes" or is_US=="n" or is_US=="no" or is_US=="NO":
+  if is_US=="N" or is_US=="No" or is_US=="n" or is_US=="no" or is_US=="NO":
     noNumbers=Input_Checker(Country)
     counter=0
     if noNumbers==False:
-      print "%s not a valid country. Please enter a valid one " % Country
+      print "%s not a valid country. Please enter a valid one " % country
       print "\n "
-      new_Country= raw_input("What state are you from? ")
-      retval=realwhereareyoufrom("N","N/A", new_Country)
+      new_country= raw_input("What state are you from? ")
+      retval=realwhereareyoufrom("N","N/A", new_country)
       return retval
     else:
       for i in len(Country):
