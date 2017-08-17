7b93295aeefde505c1e309bea4f97ce608154190
Saleh Hassen <saleh.i.hassen@gmail.com>
2017-08-17 00:07:15 -0400

    gui can go to next window and so on, still need to be able to get user input from entries tho

--- testGUI.py
+++ testGUI.py
@@ -1,46 +1,62 @@
 from appJar import gui
+class Pages(object):
+    def __init__(self):
+        self.currentPage = 0
 
-global currentPage
-currentPage = 0
+    def nextPage(self):
+        self.currentPage = self.currentPage + 1
+
+    def reset(self):
+        #not neccessary, optional
+        self.currentPage = 0
+
+class App(Pages):
+    def __init__(self):
+        Pages.__init__(self)
+        self.data = []
+        self.gui = gui()
+
+    def openWindow(self):
+        if(self.currentPage == 0):
+            welcomeMenu()
+        elif(self.currentPage == 1):
+            firstPage()
+        elif(self.currentPage == 2):
+            secondPage()
+        else:
+            print "ERROR"
+            self.currentPage = self.currentPage -1
+        #now adds a page
+        self.nextPage()
 
 def press(button): #Change this function later, bugs present, can't access the variable in line 3 which I'm trying to use on line 11,12,16,17 and 19
     if button == "Quit":
         print "Exiting"
-        app.stop()
+        majorDecisions.gui.stop()
     elif button == "Start":
-        currentPage = currentPage + 1
-        print currentPage
-        app.stop()
-        firstPage()
+        majorDecisions.gui.stop()
+        majorDecisions.nextPage()
+        majorDecisions.openWindow()
     elif button == "Next":
-        currentPage = currentPage + 1
-        print currentPage
-        app.stop()
-        selectPage(currentPage)
+        majorDecisions.gui.stop()
+        majorDecisions.nextPage()
+        majorDecisions.openWindow()
 
-
-
-def selectPage(pageNo):
-    if (pageNo == 2):
-        secondPage()
-    elif (pageNo == 3):
-        thirdPage()
-    else:
-        print 'ERROR'
 
 
 def firstPage():
-    app = gui()
-    app.startLabelFrame("First Question")
-    app.setFont(20)
-    app.addLabel('q1', '1. What is your intended major?')
-    app.addEntry('e1')
-    app.addButtons(['Quit','Next'],press)
-    app.stopLabelFrame()
-    app.go()
+    majorDecisions.gui = gui()
+    majorDecisions.gui.startLabelFrame("First Question")
+    majorDecisions.gui.setFont(20)
+    majorDecisions.gui.addLabel('q1', '1. What is your intended major?')
+    majorDecisions.gui.addEntry('e1')
+    majorDecisions.gui.addButtons(['Quit','Next'],press)
+    majorDecisions.gui.stopLabelFrame()
+    majorDecisions.gui.go()
 
 def secondPage():
-    app = gui()
+    majorDecisions.gui = gui()
+    app = majorDecisions.gui
     app.startLabelFrame("Second Question")
     app.setFont(20)
     app.addLabel('q2', '2. What state or country did you reside in?')
@@ -54,13 +70,24 @@ def secondPage():
     print "DEBUG: entry1 is " + entry1 #current issue entry1 is returned as an empty string
     print entry1                        #FIX LATER
 
+def welcomeMenu():
+    app = gui()
+    app.startLabelFrame("Welcome Menu")
+    app.setFont(20)
+    app.addLabel("t1","Welcome to...\n Major Decisions", 0, 0)
+    app.addButtons(["Start","Quit"],press)
+    app.stopLabelFrame()
+    #runs the app
+    app.go()
 
-app = gui()
-app.startLabelFrame("Welcome Menu")
-app.setFont(20)
-app.addLabel("t1","Welcome to...\n Major Decisions", 0, 0)
-app.addButtons(["Start","Quit"],press)
-app.stopLabelFrame()
-
-#runs the app
-app.go()
+majorDecisions = App()
+majorDecisions.openWindow()
+# app = gui()
+# app.startLabelFrame("Welcome Menu")
+# app.setFont(20)
+# app.addLabel("t1","Welcome to...\n Major Decisions", 0, 0)
+# app.addButtons(["Start","Quit"],press)
+# app.stopLabelFrame()
+#
+# #runs the app
+# app.go()
