e197f290497113976540b15505f39713f79699c0
Kwami2 <akk83@Cornell.edu>
2017-08-21 00:28:18 -0400

    Changed it to be friendly with the mainbash.py

--- realwhereareyoufromQ.py
+++ realwhereareyoufromQ.py
@@ -1,48 +1,90 @@
 from random import uniform
-def realwhereareyoufromQ(is_US,State,Country):
-  if is_US=="Y" or is_US=="Yes" or is_US=="y" or is_US=="yes" or is_US=="YES":
+from Input_Checker import Input_Checker 
+#CHANGE MAKE THE Is_US set to to True False/. Do Smart list 
+def realwhereareyoufromQ():
+  America_or_nah=raw_input("Are you from the United states?\n")
+  America_or_nah=America_or_nah.strip()
+  yes_phrases = ["Y","Yes","y","yes","YES","YAS","AMERICAAAAAA!!!"]
+  no_phrases =  ["N","No","n","no","NO","Nah","Nope"]
+  is_US=False
+  CorrectInput=False
+  while CorrectInput== False:
+    if America_or_nah in yes_phrases:
+      is_US=True
+      CorrectInput=True
+    elif America_or_nah in no_phrases:
+      is_US=False
+      CorrectInput=True
+    else:
+      print "Please answer yes or no"
+      America_or_nah=raw_input("Are you from the United states?")
+      
+  if is_US==True:
+    State=raw_input("What state are you from?")
     noNumbers=Input_Checker(State)
     counter=0
     if noNumbers==False:
-      print "%s not a valid state. Please enter a valid one " % State
-      print " \n "
+      print "%s not a valid state. Please enter a valid one \n " % State
       new_state= raw_input("What state are you from? ")
       retval=realwhereareyoufrom("Y",new_state,"USA")
       return retval
     else:
-      for i in len(State):
-        if state[i]=="a":
+      for i in range(len(State)):
+        if State[i]=="a":
           counter=counter+1
-          if counter==1:
-            return uniform(.0,.15)
-          elif counter==2:
-            return uniform(.15,.25)
-          elif counter==3:
-            return uniform(.0,.3)
-          elif couner==4:
-            return uniform(.25,.3)
-          else:
-            return uniform(.0,.3)
-  if is_US=="N" or is_US=="No" or is_US=="n" or is_US=="no" or is_US=="NO":
+      if counter==1:
+        ans=uniform(.0,.15)
+        print ans
+        return ans 
+      elif counter==2:
+        ans=uniform(.15,.25)
+        print ans
+        return ans 
+      elif counter==3:
+        ans=uniform(.0,.3)
+        print ans
+        return ans 
+      elif couner==4:
+        ans=uniform(.25,.3)
+        print ans 
+        return ans 
+      else:
+        ans=uniform(.0,.3)
+        print ans
+        return ans
+        
+  if is_US==False:
+    Country=raw_input("What country are you from?")
     noNumbers=Input_Checker(Country)
     counter=0
     if noNumbers==False:
-      print "%s not a valid country. Please enter a valid one " % country
-      print "\n "
-      new_country= raw_input("What state are you from? ")
+      print "%s not a valid country. Please enter a valid one \n " % country
+      new_country= raw_input("Test:What country are you from? ")
       retval=realwhereareyoufrom("N","N/A", new_country)
       return retval
     else:
-      for i in len(Country):
+      for i in range(len(Country)):
         if Country[i]=="a":
           counter=counter+1
-          if counter==1:
-            return uniform(.0,.15)
-          elif counter==2:
-            return uniform(.15,.25)
-          elif counter==3:
-            return uniform(.0,.3)
-          elif couner==4:
-            return uniform(.25,.3)
-          else:
-            return uniform(.0,.3)
+      if counter==1:
+        ans=uniform(.0,.15)
+        print ans
+        return ans 
+      elif counter==2:
+        ans=uniform(.15,.25)
+        print ans 
+        return ans 
+      elif counter==3:
+        ans=uniform(.0,.3)
+        print ans
+        return ans 
+      elif couner==4:
+        ans=uniform(.25,.3)
+        print ans
+        return ans
+      else:
+        ans=uniform(.0,.3)
+        print ans 
+        return 
+          
+realwhereareyoufromQ()
