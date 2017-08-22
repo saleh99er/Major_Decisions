ec0c7b54ec49ddf31bccae2802c4b20501023059
Kwami2 <akk83@Cornell.edu>
2017-08-22 13:58:27 -0400

    Please look and test to see if there are any errors. Try to break it

--- realwhereareyoufromQ.py
+++ realwhereareyoufromQ.py
@@ -1,8 +1,5 @@
 from random import uniform
 from Input_Checker import Input_Checker
-from realwhereareyoufromQ import realwhereareyoufromQ
-
-#CHANGE MAKE THE Is_US set to to True False/. Do Smart list 
 def realwhereareyoufromQ():
   America_or_nah=raw_input("Are you from the United states?\n")
   America_or_nah=America_or_nah.strip()
@@ -22,70 +19,68 @@ def realwhereareyoufromQ():
       America_or_nah=raw_input("Are you from the United states?")
       
   if is_US==True:
-    State=raw_input("What state are you from?")
-    noNumbers=Input_Checker(State)
+    noNumbers=False 
+    while noNumbers==False:
+      State=raw_input("What state are you from? \n")
+      noNumbers=Input_Checker(State)
+      if noNumbers==False:
+        print "%s not a valid state. Please enter a valid one \n " % State
+    for i in range(4):
+      print "Calculating Value...\n" 
     counter=0
-    if noNumbers==False:
-      print "%s not a valid state. Please enter a valid one \n " % State
-      new_state= raw_input("What state are you from? ")
-      retval=realwhereareyoufrom("Y",new_state,"USA")
-      return retval
+    for i in range(len(State)):
+      if State[i]=="a":
+        counter=counter+1
+    if counter==1:
+      ans=uniform(.0,.15)
+      print ans
+      return ans 
+    elif counter==2:
+      ans=uniform(.15,.25)
+      print ans
+      return ans 
+    elif counter==3:
+      ans=uniform(.0,.3)
+      print ans
+      return ans 
+    elif couner==4:
+      ans=uniform(.25,.3)
+      print ans 
+      return ans 
     else:
-      for i in range(len(State)):
-        if State[i]=="a":
-          counter=counter+1
-      if counter==1:
-        ans=uniform(.0,.15)
-        print ans
-        return ans 
-      elif counter==2:
-        ans=uniform(.15,.25)
-        print ans
-        return ans 
-      elif counter==3:
-        ans=uniform(.0,.3)
-        print ans
-        return ans 
-      elif couner==4:
-        ans=uniform(.25,.3)
-        print ans 
-        return ans 
-      else:
-        ans=uniform(.0,.3)
-        print ans
-        return ans
+      ans=uniform(.0,.3)
+      print ans
+      return ans
         
   if is_US==False:
-    Country=raw_input("What country are you from?")
-    noNumbers=Input_Checker(Country)
+    noNumbers=False
+    while noNumbers==False:
+      Country=raw_input("What country are you from? \n")
+      noNumbers=Input_Checker(Country)
+    for i in range(4):
+      print "Calculating Value...\n" 
     counter=0
-    if noNumbers==False:
-      print "%s not a valid country. Please enter a valid one \n " % Country
-      retval=realwhereareyoufrom()
-      print retval
-      return retval
+    for i in range(len(Country)):
+      if Country[i]=="a":
+        counter=counter+1
+    if counter==1:
+      ans=uniform(.0,.15)
+      print ans
+      return ans 
+    elif counter==2:
+      ans=uniform(.15,.25)
+      print ans 
+      return ans 
+    elif counter==3:
+      ans=uniform(.0,.3)
+      print ans
+      return ans 
+    elif counter==4:
+      ans=uniform(.25,.3)
+      print ans
+      return ans
     else:
-      for i in range(len(Country)):
-        if Country[i]=="a":
-          counter=counter+1
-      if counter==1:
-        ans=uniform(.0,.15)
-        print ans
-        return ans 
-      elif counter==2:
-        ans=uniform(.15,.25)
-        print ans 
-        return ans 
-      elif counter==3:
-        ans=uniform(.0,.3)
-        print ans
-        return ans 
-      elif couner==4:
-        ans=uniform(.25,.3)
-        print ans
-        return ans
-      else:
-        ans=uniform(.0,.3)
-        print ans 
-        return 
+      ans=uniform(.0,.3)
+      print ans 
+      return 
 realwhereareyoufromQ()
