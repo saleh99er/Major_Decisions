8e9a190223a1260bc92fb088859bff6eb30afae5
Atsutse Kludze <30853982+Atsutse@users.noreply.github.com>
2017-08-25 08:01:15 -0400

    still need to fix

--- intended_dorm.py
+++ intended_dorm.py
@@ -1,30 +1,27 @@
 from random import uniform
 from random import random
 from Input_Checker import Input_Checker 
-def intended_dorm(dorm_input):
-  THESE LIST VARIABLES (ALL OF THEM) SHOULD TECHNICALLY BE IN ALL CAPS BECAUSE THEY ARE CONSTANTS, PLEASE MAKE THAT CHANGE BUT IT IS NOT IMPORTANT
-  EX. A = ['mews','mews hall']
-  a = ['mews','mews hall']
-  b = ['townhouse','townhouse community']
-  c = ['balch','balch hall']
-  d = ['clara dickson','clara dickson hall']
-  e = ['bauer','court-kay-bauer','court kay bauer','court-kay-bauer hall','court kay bauer hall']
-  f = ['high rise','high rise','high rise #5','high rise #5']
-  g = ['jameson','jameson hall']
-  h = ['low rise','low rise','low-rise #6','low-rise #6','low rise #6','low rise #6','low-rise #6','low-rise #6']
-  i = ['low-rise #7','low-rise #7','low rise #7','low rise #7','low-rise #7','low-rise #7']
-  j = ['mary donlon','mary donlon hall','mary','mary hall','donlon','donolon hall', 'egg']
-  k = ['akwe:kon','akwekon','akwe kon','akwe-kon']
-  l = ['ecology house','ecology-house','the ecology-house','the ecology house']
-  m = ['holland international living center','the holland international living center', 'hilc', 'the hilc']
-  n = ['just about music','jam']
-  o = ['latino living center','the latino living center','llc']
-  p = ['multicultural living learning unit','multicultural living learning unit','the multicultural living learning unit','mcllu']
-  q = ['risley residential college','the risley residential college','risley']
-  r = ['ujamaa residential college','uj','ujamaa','the ujamaa residential college']
-  s = ['language house','the language house','lh']
-  master_dorm_list=[a,b,c,d,e,g,h,i,j,k,l,m,n,o,p,q,r,s]
-  
+def intended_dorm(dorm):
+  A = ['mews','mews hall']
+  B = ['townhouse','townhouse community']
+  C = ['balch','balch hall']
+  D = ['clara dickson','clara dickson hall']
+  E = ['bauer','court-kay-bauer','court kay bauer','court-kay-bauer hall','court kay bauer hall']
+  F = ['high rise','high rise','high rise #5','high rise #5']
+  G = ['jameson','jameson hall']
+  H = ['low rise','low rise','low-rise #6','low-rise #6','low rise #6','low rise #6','low-rise #6','low-rise #6']
+  I = ['low-rise #7','low-rise #7','low rise #7','low rise #7','low-rise #7','low-rise #7']
+  J = ['mary donlon','mary donlon hall','mary','mary hall','donlon','donolon hall', 'egg']
+  K = ['akwe:kon','akwekon','akwe kon','akwe-kon']
+  L = ['ecology house','ecology-house','the ecology-house','the ecology house']
+  M = ['holland international living center','the holland international living center', 'hilc', 'the hilc']
+  N = ['just about music','jam']
+  O = ['latino living center','the latino living center','llc']
+  P = ['multicultural living learning unit','multicultural living learning unit','the multicultural living learning unit','mcllu']
+  Q = ['risley residential college','the risley residential college','risley']
+  R = ['ujamaa residential college','uj','ujamaa','the ujamaa residential college']
+  S = ['language house','the language house','lh']
+  master_dorm_list=[A,B,C,D,E,G,H,I,J,K,L,M,N,O,P,Q,R,S]
   # This while loops keeps running until the user gives a valid input.
 
       #is_dorm_name_in_list=False
@@ -32,8 +29,8 @@ def intended_dorm(dorm_input):
   # The bounds of psuedo-randomness are 0 and .1.
   # The for-loops checks for whether the user's input matches any of the dorms names.
   # As the check moves to the next 'dorm_building', the bounds of randomness are shifted by the variable "increment"
-    #Ex: when dorm_building = a, the bound of randomness are 0 and .1
-    # when dorm_building = b, the bounds of randomness are the [value of increment] and [.1 + value of increment]
+    #Ex: when dorm_building = A, the bound of randomness are 0 and .1
+    # when dorm_building = B, the bounds of randomness are the [value of increment] and [.1 + value of increment]
   lower_bound = 0
   higher_bound = .1
   increment = float(.2/(len(master_dorm_list)-1))
@@ -41,7 +38,7 @@ def intended_dorm(dorm_input):
     for dorm_name in dorm_building:
       if dorm == dorm_name:
         ans=uniform(lower_bound,higher_bound)
-        print "Value Generated: " + str(ans)
+        #print "Value Generated: " + str(ans)
         return ans
       else:
         lower_bound =  lower_bound + increment
